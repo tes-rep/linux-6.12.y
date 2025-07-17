ee961fc8a3bad6055435ad5396631557b243bec4
hj-wrt <mincoek76@gmail.com>
2025-07-18 06:45:44 +0700

    Update gl_p2p_cfg80211.c

--- drivers/net/wireless/mediatek/mt7668/os/linux/gl_p2p_cfg80211.c
+++ drivers/net/wireless/mediatek/mt7668/os/linux/gl_p2p_cfg80211.c
@@ -1913,14 +1913,14 @@ struct cfg80211_beacon_data {
 #endif
 
 int mtk_p2p_cfg80211_change_beacon(struct wiphy *wiphy, struct net_device *dev,
-#if (LINUX_VERSION_CODE < KERNEL_VERSION(6, 7, 0))
+#if (LINUX_VERSION_CODE < KERNEL_VERSION(6, 12, 0))
 	struct cfg80211_beacon_data *info
 #else
 	struct cfg80211_ap_update *info
 #endif
 )
 {
-#if (LINUX_VERSION_CODE < KERNEL_VERSION(6, 7, 0))
+#if (LINUX_VERSION_CODE < KERNEL_VERSION(6, 12, 0))
 	struct cfg80211_beacon_data *beacon = info;
 #else
 	struct cfg80211_beacon_data *beacon = &info->beacon;
