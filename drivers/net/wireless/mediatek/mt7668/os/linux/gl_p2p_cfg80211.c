aa2e592a51330aeef25e9234ad5023d92ddf6d17
hj-wrt <mincoek76@gmail.com>
2025-07-19 14:58:48 +0700

    Update gl_p2p_cfg80211.c

--- drivers/net/wireless/mediatek/mt7668/os/linux/gl_p2p_cfg80211.c
+++ drivers/net/wireless/mediatek/mt7668/os/linux/gl_p2p_cfg80211.c
@@ -1705,7 +1705,7 @@ static int mtk_p2p_cfg80211_start_radar_detection_impl(struct wiphy *wiphy, stru
 
 #if KERNEL_VERSION(3, 15, 0) <= CFG80211_VERSION_CODE
 int mtk_p2p_cfg80211_start_radar_detection(struct wiphy *wiphy, struct net_device *dev,
-                                           struct cfg80211_chan_def *chandef, u32 freq, int cac_time_ms);
+                                           struct cfg80211_chan_def *chandef, u32 freq, int cac_time_ms)
 
 {
 	return mtk_p2p_cfg80211_start_radar_detection_impl(
@@ -1713,7 +1713,7 @@ int mtk_p2p_cfg80211_start_radar_detection(struct wiphy *wiphy, struct net_devic
 }
 #else
 int mtk_p2p_cfg80211_start_radar_detection(struct wiphy *wiphy, struct net_device *dev,
-					struct cfg80211_chan_def *chandef, u32 freq, int cac_time_ms);
+					struct cfg80211_chan_def *chandef, u32 freq, int cac_time_ms)
 {
 	return mtk_p2p_cfg80211_start_radar_detection_impl(
 			wiphy, dev, chandef, IEEE80211_DFS_MIN_CAC_TIME_MS);
